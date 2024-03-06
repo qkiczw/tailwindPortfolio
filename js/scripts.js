8a1080af1b6b1e3c6b8bb496e62a219c1354d56d
Łukasz Sztormowski <lukaszsztormowski@gmail.com>
2024-03-06 19:05:39 +0100

    Add test code to detect Scroll position of the section title and change a color of a nav icon in the right desktop menu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,23 @@
 console.log("Hello World!");
+
+// nav active icon code
+const navIconColorHandle = () => {
+  const sectionTitles = document.querySelectorAll(".section__title");
+  const navIcons = document.querySelectorAll(".nav__item");
+
+  const scrollPosition = window.scrollY;
+  const scrollThreshold = window.innerHeight / 2;
+
+  //   Napisz taki kod żeby sprawdzało każdy section title od góry i jeżeli
+  //   jakiś section ma mniej nic scroll treshold to ikona o takim samym dataset robi się zielona!!!
+  sectionTitles.forEach((title) => {
+    const testtop = title.getBoundingClientRect();
+    if (testtop.top < scrollThreshold) {
+      navIcons[0].classList.add("icon--active");
+    } else {
+      navIcons[0].classList.remove("icon--active");
+    }
+  });
+};
+
+window.addEventListener("scroll", navIconColorHandle);
